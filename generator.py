7838df77b2e2617bc927b098eb52894ca824a659
Juan Mellano <juanegameryt@gmail.com>
2023-03-29 21:21:49 -0300

    Update V1.2
    More, more updates

--- generator.py
+++ generator.py
@@ -1,41 +1,54 @@
 import itertools
 import random
 
+# Preguntar si se incluirán caracteres especiales
+caracteres_especiales = input("¿You want to include special characters? (s/n): ")
+
+if caracteres_especiales.lower() == "s":
+    # Definir los caracteres que se combinarán, incluyendo caracteres especiales
+    caracteres = "abcdefghijklmnopqrstuvwxyz!#$%&()*+,-./:;<=>?@[\\]^_`{|}~"
+    caracteres_mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numeros = "0123456789"
+else:
+    # Definir los caracteres que se combinarán, sin caracteres especiales
+    caracteres = "abcdefghijklmnopqrstuvwxyz"
+    caracteres_mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numeros = "0123456789"
+
 # Pedir la cantidad de letras mayúsculas y minúsculas
-cant_mayus = int(input("Enter the number of capital letters you want to include: "))
-cant_minus = int(input("Enter the number of lowercase letters you want to include: "))
+cant_mayus = int(input("Enter the amount of the number of capital letters you want to include: "))
+cant_minus = int(input("Enter the amount of the number of lowercase letters you want to include: "))
 
 # Pedir la cantidad de números que se incluirán
-cant_numeros = int(input("Enter the number of numbers you want to include: "))
+cant_numeros = int(input("Enter the amount of the numbers of numbers you want to include: "))
 
-# Definir los caracteres que se combinarán
-caracteres = "abcdefghijklmnopqrstuvwxyz"
-caracteres_mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numeros = "0123456789"
+# Pedir la cantidad de combinaciones a generar
+cant_combinaciones = int(input("Enter the amount of the number of combinations you want to generate: "))
 
 # Calcular la longitud total de cada combinación
 longitud = cant_mayus + cant_minus + cant_numeros
 
-# Elegir al azar los caracteres que se utilizarán en cada combinación
-caracteres_comb = [random.choice(caracteres) for i in range(longitud-cant_mayus-cant_minus-cant_numeros)]
-caracteres_mayus_comb = [random.choice(caracteres_mayus) for i in range(cant_mayus)]
-caracteres_minus_comb = [random.choice(caracteres) for i in range(cant_minus)]
-numeros_comb = [random.choice(numeros) for i in range(cant_numeros)]
+# Generar múltiples combinaciones
+for i in range(cant_combinaciones):
+    # Elegir al azar los caracteres que se utilizarán en cada combinación
+    caracteres_comb = [random.choice(caracteres) for i in range(longitud-cant_mayus-cant_minus-cant_numeros)]
+    caracteres_mayus_comb = [random.choice(caracteres_mayus) for i in range(cant_mayus)]
+    caracteres_minus_comb = [random.choice(caracteres) for i in range(cant_minus)]
+    numeros_comb = [random.choice(numeros) for i in range(cant_numeros)]
 
-# Combinar todos los caracteres
-caracteres_comb.extend(caracteres_mayus_comb)
-caracteres_comb.extend(caracteres_minus_comb)
-caracteres_comb.extend(numeros_comb)
+    # Combinar todos los caracteres
+    caracteres_comb.extend(caracteres_mayus_comb)
+    caracteres_comb.extend(caracteres_minus_comb)
+    caracteres_comb.extend(numeros_comb)
 
-# Mezclar los caracteres para generar combinaciones únicas
-random.shuffle(caracteres_comb)
+    # Mezclar los caracteres para generar combinaciones únicas
+    random.shuffle(caracteres_comb)
 
-# Convertir la lista de caracteres en una cadena
-combinacion = ''.join(caracteres_comb)
+    # Convertir la lista de caracteres en una cadena
+    combinacion = ''.join(caracteres_comb)
 
-# Escribir la combinación en el archivo "generated.txt"
-with open("generated.txt", "w") as archivo:
-    archivo.write(combinacion + "\n")
+    # Escribir la combinación en el archivo "generated.txt"
+    with open("generated.txt", "a") as archivo:
+        archivo.write(combinacion + "\n")
 
-#El creador es español, por si quieres leer la descripcion y no entiendes (para ingleses)
-#The creator is Spanish, in case you want to read the description and don't understand (for English)
+print(f"Generated {cant_combinaciones} combinations 'generated.txt'.")
--- termux.sh
+++ termux.sh
@@ -1,22 +1,29 @@
-pkg update -y
+pkg update
 pkg upgrade -y
 pkg install python
 pip install --upgrade pip
 pkg install lolcat
 sleep 4
-figlet JuanE |lolcat -F 1
-clear
-figlet J-Tools |lolcat -F 1
-clear
-echo "Speaking? Hablas? (1) English o (2) Español"
+echo "JuanE" |lolcat -F 1
+echo " ¿Que idioma Hablas? | What is your language? "
+echo " (1) English | (2) Español"
 read opcion
 
 if [ $opcion -eq 1 ]; then
+    clear
+    echo "JuanE" | lolcat -F 1
     python generator.py
 elif [ $opcion -eq 2 ]; then
+    clear
+    echo "JuanE" | lolcat -F 1
     python generador.py
 else
+    clear
+    echo "JuanE" | lolcat -F 1
     echo "Opción inválida. Por favor ingresa 1 para inglés o 2 para español."
     echo "Invalid option. Please enter 1 for English or 2 for Spanish."
 fi
 
+sleep 1
+exit
+esac
