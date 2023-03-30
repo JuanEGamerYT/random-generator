4f4f58431215b01cb0dbd1d6414de4ff880606e5
Juan Mellano <juanegameryt@gmail.com>
2023-03-30 13:24:49 -0300

    Fixed V1.2.2

--- setup.sh
+++ setup.sh
@@ -1,8 +1,30 @@
+figlet "JuanE" | lolcat -F 1
+echo "Installing"
+clear
+echo "Installing."
+clear
+echo "Installing.."
+clear
+echo "Installing..."
+echo "Start."
 pkg update
 pkg upgrade -y
 pkg install python
 pip install --upgrade pip
 pkg install lolcat -y
 pkg install figlet -y
+clear
+echo "Reqisites: OK" | lolcat
+cd ..
+rm -rf Generator
+clear
+echo "Updating?: ..."
+clear
+figlet "JuanE" | lolcat -F 1
+pkg install git -y
+clear
+git clone https://github.com/JuanEGamerYT/Generator
+clear
+echo "Updated: OK to last version avaible" | lolcat
 clear
 sleep 1
