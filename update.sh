7525f6d0170328f132294cf0d0269a7a4c182f97
Juan Mellano <juanegameryt@gmail.com>
2023-03-30 13:19:18 -0300

    Fixed V1.2.2

--- update.sh
+++ update.sh
@@ -8,6 +8,6 @@ pkg install git -y
 git clone https://github.com/JuanEGamerYT/Generator
 clear
 figlet "JuanE" | lolcat -F 1
-cd Generator
 echo "Actualizado a la última version | Updated to last version" | lolcat
+cd
 sleep 1
